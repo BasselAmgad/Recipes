1a44b093ed43b833042347aa892c1b91630cf2c4
Bassel <Bassel@BASSEL>
2022-06-27 19:57:50 +0200

    Add project files.

---
+++ Recipes/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Spectre.Console;
+using System.Text.Json;
+using System.IO;
+string curFile = @"C:\Users\RS3\source\repos\Recipes\Recipes\recipes.json";
+
+AnsiConsole.Markup("[underline red]Hello[/] World!");
+
+Categories[] c = { Categories.Breakfast };
+
+Recipe recipe1 = new Recipe("Scrambeled Eggs 1",
+    "salt, pepper, eggs, olive oil",
+    "Add Olive oil to the pan on low heat then put the crach the egg and put it",
+     c);
+Recipe recipe2 = new Recipe("Scrambeled Eggs 2",
+    "salt, pepper, eggs, olive oil",
+    "Add Olive oil to the pan on low heat then put the crach the egg and put it",
+     c);
+Recipe recipe3 = new Recipe("Scrambeled Eggs 3",
+    "salt, pepper, eggs, olive oil",
+    "Add Olive oil to the pan on low heat then put the crach the egg and put it",
+     c);
+Recipe recipe4 = new Recipe("Scrambeled Eggs 4",
+    "salt, pepper, eggs, olive oil",
+    "Add Olive oil to the pan on low heat then put the crach the egg and put it",
+     c);
+Recipe[] recipes = { recipe1, recipe2, recipe3, recipe4 };
+var json = JsonSerializer.Serialize(recipes);
+
+
+Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+if (!File.Exists(curFile))
+{
+    File.WriteAllText(curFile, json);
+}
---
+++ Recipes/Recipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Recipe
+{
+    public string Title { get; set; }
+    public string Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public Categories[] Categories { get; set; }
+
+    public Recipe(string title, string ingredients, string instructions, Categories[] categories)
+    {
+        Title = title;
+        Ingredients = ingredients;
+        Instructions = instructions;
+        Categories = categories;
+    }
+
+}
+
+enum Categories
+{
+    Breakfast,
+    Lunch,
+    Dinner,
+    Snack
+}
